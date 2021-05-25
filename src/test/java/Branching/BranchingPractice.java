eb0ae93805738cf64d097871924e0c2e7cd98530
Vladislav <vladislav13457@gmail.com>
2021-05-25 19:29:26 -0400

    first commit in review1 branch

--- src/test/java/Branching/BranchingPractice.java
+++ src/test/java/Branching/BranchingPractice.java
@@ -2,6 +2,8 @@ package Branching;
 
 public class BranchingPractice {
     public static void main(String[] args) {
-// created new projectK
+// created new project
+        //enabl
+        // reviewing on new branch
     }
 }
