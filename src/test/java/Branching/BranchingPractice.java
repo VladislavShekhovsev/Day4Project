b1c6370739d47eb1b88b0a9ccf0812b23b30276b
Vladislav <vladislav13457@gmail.com>
2021-05-25 19:30:50 -0400

    second commit in review1 branch

--- src/test/java/Branching/BranchingPractice.java
+++ src/test/java/Branching/BranchingPractice.java
@@ -3,7 +3,9 @@ package Branching;
 public class BranchingPractice {
     public static void main(String[] args) {
 // created new project
-        //enabl
+        //enabled version control
+        // added .gitignore with .idea, target folder and iml file
+
         // reviewing on new branch
     }
 }
