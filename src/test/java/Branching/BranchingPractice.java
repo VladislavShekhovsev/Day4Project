2dba6d3cc9b58e857884e5923103062f7792406b
Vladislav <vladislav13457@gmail.com>
2021-05-25 19:32:46 -0400

    third commit on review1 branch

--- src/test/java/Branching/BranchingPractice.java
+++ src/test/java/Branching/BranchingPractice.java
@@ -5,7 +5,8 @@ public class BranchingPractice {
 // created new project
         //enabled version control
         // added .gitignore with .idea, target folder and iml file
-
+        // made initial commit by keeping track of all files
+        // shared project on github
         // reviewing on new branch
     }
 }
