f9a865fedb1032eab099d3ab1bebe94eddcff304
Vladislav <vladislav13457@gmail.com>
2021-05-25 21:45:08 -0400

    TC02  part 2 continue of TC02

--- src/test/java/Branching/TC02Part2.java
+++ src/test/java/Branching/TC02Part2.java
@@ -3,5 +3,6 @@ package Branching;
 public class TC02Part2 {
     public static void main(String[] args) {
         System.out.println("started part 2 of TC02");
+        System.out.println("continue working on TC01");
     }
 }
