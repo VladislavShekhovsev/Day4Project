7ae29afbe2c8bd4c5cc02ea6a85d3e03ae101052
Vladislav <vladislav13457@gmail.com>
2021-05-25 21:45:41 -0400

    TC02  part 2 finished

--- src/test/java/Branching/TC02Part2.java
+++ src/test/java/Branching/TC02Part2.java
@@ -4,5 +4,6 @@ public class TC02Part2 {
     public static void main(String[] args) {
         System.out.println("started part 2 of TC02");
         System.out.println("continue working on TC01");
+        System.out.println("finished TC02 part 2");
     }
 }
