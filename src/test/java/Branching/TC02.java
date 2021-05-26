dd840579226b368f577a782db2f0ee4e3e947787
Vladislav <vladislav13457@gmail.com>
2021-05-25 21:42:39 -0400

    TC02 continue

--- src/test/java/Branching/TC02.java
+++ src/test/java/Branching/TC02.java
@@ -3,5 +3,7 @@ package Branching;
 public class TC02 {
     public static void main(String[] args) {
         System.out.println("My first file");
+        System.out.println("my second file");
+        System.out.println("Some more file");
     }
 }
